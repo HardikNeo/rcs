ca6b6d3eff4fe882de25c1f8c04701ff3188e5af
Hardik <neopanehardik630@gmail.com>
2024-05-08 23:32:08 +0545

    played counter and draw

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 var computer = 0;
 var player = 0;
 var won = 0;
-const nbtn = document.getElementById("new");
+var played = 0;
 const msg = document.getElementById("msg");
+const counter = document.getElementById("count");
+const pcounter = document.getElementById("p-count");
 function play(p) {
   if (player != 0) {
     alert("Please Start A New Game!");
   } else {
+    played += 1;
     player = p;
     computer = Math.floor(Math.random() * 3) + 1;
     console.log(player, computer);
     if (player === computer) {
-      msg.textContent = "It resulted in a draw!";
+      msg.textContent = "It's a draw!";
     } else if (player === 1) {
       if (computer === 2) {
         msg.textContent = "Computer chose paper, You Lost!";
@@ -35,9 +38,12 @@ function play(p) {
       }
     }
   }
+  pcounter.textContent = played;
+  counter.textContent = won;
 }
 
 function newg() {
   player = 0;
   computer = 0;
+  msg.textContent = "Please Make a Move";
 }
